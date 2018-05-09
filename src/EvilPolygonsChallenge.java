0639c586a509b0dc445ad69a19e016675a816e1d
Rosenzweig.Ashton <Rosenzweig.Ashton@SEMS-259-32.SEMS259.LAB>
2018-05-09 14:41:48 -0700

    CircleRing, EvilPolygons, GoofyNames

--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -5,23 +5,64 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
+
 	public static void main(String[] args) {
-		//1. Create a robot
+		
+		// 1. Create a robot
+		Robot bob = new Robot();
+		bob.setWindowSize(2000, 2000);
+		int i = 0;
+		int j = 0;
+		int k = 0;
 
-		//2. Set the speed to 100
+		// 2. Set the speed to 100
+		bob.setSpeed(10);
+		int colorChoice = JOptionPane.showOptionDialog(null, "What color would you like?", "Color Choice",
+				JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null,
+				new String[] { "Red", "Green", "Blue" }, 0);
+		if (colorChoice == 0) {
+			i = 255;
+			j = 0;
+			k = 0;
+		}
+		if (colorChoice == 1) {
+			i = 0;
+			j = 255;
+			k = 0;
+		}
+		if (colorChoice == 2) {
+			i = 0;
+			j = 0;
+			k = 255;
+		}
+		// 3. Set the pen color of the robot according to the value of the colorChoise
+		// variable
+		bob.setPenColor(i, j, k);
+		// 4. Ask the use how many polygons they want to be drawn.
+		String polygon = JOptionPane.showInputDialog("How many polygons do you want drawn?");
+		int polygons = Integer.parseInt(polygon);
+		// 5. Use the robot to draw the number of polygons the user requested.
+		for (int l = 0; l < polygons; l++) {
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
-		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
-		//6. Make it so your shapes do not overlap
+			bob.penDown();
+			bob.setAngle(270);
+			bob.move(100);		
+			bob.turn(90);
+			bob.move(100);
+			bob.turn(90);
+			bob.move(100);
+			bob.turn(90);
+			bob.move(100);
+			bob.penUp();
+			bob.move(150);
 
-		//7. Challenge: add more colors to the Option Dialog.
+			
+		}
+		// 6. Make it so your shapes do not overlap
+
+		// 7. Challenge: add more colors to the Option Dialog.
 	}
 }
 
+// TODO Auto-generated method stub
